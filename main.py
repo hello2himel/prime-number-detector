088db5fe22d1b378e7c2cf61fe478aedc38464a9
Himel Das <99470657+hello2himel@users.noreply.github.com>
2023-08-25 17:20:41 +0600

    update

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 print('You can check if a number is prime or not using this tool.')
-num = int(input('Please type the number that you want to check: '))
-print('Checking your number...')
+num = input('Please type the number that you want to check: ')
+if float(num) % 1 == 0:
+    print('Checking your number...')
 
-if num < 2:
-    statement = f'{num} is not a prime number.'
-elif num == 2:
-    statement = f'{num} is a prime number'
-else:
-    is_prime = True
-    for i in range(2, num):
-        if num % i == 0:
-            is_prime = False
-            break
-    if is_prime:
+    if int(num) < 2:
+        statement = f'{num} is not a prime number.'
+    elif int(num) == 2:
         statement = f'{num} is a prime number'
     else:
-        statement = f'{num} is not a prime number'
+        is_prime = True
+        for i in range(2, int(num)):
+            if int(num) % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            statement = f'{num} is a prime number'
+        else:
+            statement = f'{num} is not a prime number'
+
+else:
+    statement = f'{num} is not an int, therefore {num} is not a prime number.'
 
 print(statement)
